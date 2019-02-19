c36952f90a0fb99e5be2e4ed48a51089c58e3158
wada-don <rw.ryu.18.wr@gmail.com>
2019-02-19 22:15:59 +0900

    Initial Commit

---
+++ SafariViewSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SafariViewSample
+//
+//  Created by Ryusei Wada on 2019/02/19.
+//  Copyright © 2019 Ryusei Wada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ SafariViewSampleTests/SafariViewSampleTests.swift
@@ -0,0 +1,34 @@
+//
+//  SafariViewSampleTests.swift
+//  SafariViewSampleTests
+//
+//  Created by Ryusei Wada on 2019/02/19.
+//  Copyright © 2019 Ryusei Wada. All rights reserved.
+//
+
+import XCTest
+@testable import SafariViewSample
+
+class SafariViewSampleTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
