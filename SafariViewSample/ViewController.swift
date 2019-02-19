fad0b641b49b91c5b5c8d9dcebc9b7b773c42f1c
wada-don <rw.ryu.18.wr@gmail.com>
2019-02-20 00:45:32 +0900

    search word with google

--- SafariViewSample/ViewController.swift
+++ SafariViewSample/ViewController.swift
@@ -7,14 +7,25 @@
 //
 
 import UIKit
+import SafariServices
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var searchWordTextField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func openSafari(){
+        let searchWord: String = searchWordTextField.text!
+        let url: String = "https://google.com/search?q=" + searchWord
+        let encUrl = NSURL(string: url.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!)
+        let safariViewController: SFSafariViewController = SFSafariViewController(url: encUrl! as URL)
+        present(safariViewController, animated: true, completion: nil)
+        print(encUrl!)
+    }
 
 }
 
